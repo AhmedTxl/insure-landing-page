796076da3e3d25711a78ad5235b060b699b4da44
AhmedTx <63393122+AhmedTxl@users.noreply.github.com>
2023-06-26 04:24:36 +0300

    Fixing a warning about svg naming properties

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,8 +24,8 @@ export default function Header() {
             <span className="sr-only">Open main menu</span>
             {showMenu ? (
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32">
-                <g fill="none" fill-rule="evenodd">
-                  <path fill="#FFF" stroke="#2C2830" stroke-width="1.5" d="M.75.75h30.5v30.5H.75z" />
+                <g fill="none" fillRule="evenodd">
+                  <path fill="#FFF" stroke="#2C2830" strokeWidth="1.5" d="M.75.75h30.5v30.5H.75z" />
                   <g fill="#2C2830">
                     <path d="M10.873 9.563l11.314 11.314-1.06 1.06L9.813 10.623z" />
                     <path d="M9.813 20.877L21.127 9.563l1.06 1.06-11.314 11.314z" />
@@ -34,8 +34,8 @@ export default function Header() {
               </svg>
             ) : (
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32">
-                <g fill="none" fill-rule="evenodd">
-                  <path fill="#FFF" stroke="#2C2830" stroke-width="1.5" d="M.75.75h30.5v30.5H.75z" />
+                <g fill="none" fillRule="evenodd">
+                  <path fill="#FFF" stroke="#2C2830" strokeWidth="1.5" d="M.75.75h30.5v30.5H.75z" />
                   <g fill="#2C2830">
                     <path d="M8 10h16v1.5H8zM8 15h16v1.5H8zM8 20h16v1.5H8z" />
                   </g>
